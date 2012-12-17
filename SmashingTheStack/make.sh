de3e4096b9cb543642d4dce2fec834834a94f795
Luca Mella <lucam.ko@gmail.com>
2012-12-17 09:51:00 +0100

    Revert "reference updated"
    This reverts commit 4f1a703ca880d24154b5716e2bb0b8b791c44d7b.

--- SmashingTheStack/make.sh
+++ SmashingTheStack/make.sh
@@ -1,3 +1,3 @@
 #/bin/bash
-bibtex /tmp/main.aux
-pdflatex -shell-escape -output-directory /tmp/ $*.tex && mv ./tmp/main.pdf ./main.pdf
+bibtex tmp/main.aux
+pdflatex -shell-escape -output-directory tmp/ $*.tex && mv ./tmp/main.pdf ./main.pdf
